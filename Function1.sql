270eab2a2623e9c885c50f62d903ffc95dd064aa
Manoharsd <81382879+Manoharsd@users.noreply.github.com>
2021-03-25 14:03:18 -0400

    Create Function1.sql

---
+++ Function1.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION PRDMMIS.fn_GetAnchorDt(pfrom_date DATE, pto_date DATE)
+   RETURN DATE
+IS 
+BEGIN
+   IF TRUNC(SYSDATE) BETWEEN pfrom_date AND pto_Date THEN
+      RETURN TRUNC(SYSDATE);
+   ELSIF TRUNC(SYSDATE) < pfrom_date THEN
+      RETURN pfrom_date;
+   ELSE
+      RETURN pto_date;
+   END IF;
+END; -- fn_GetAnchorDt
+/
